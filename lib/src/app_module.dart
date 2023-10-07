f074e99d5047f53897e18e7bf1888026e139eb12
Josue Silva de Morais <josuemorais@gmail.com>
2023-10-07 11:02:56 -0300

    Versão 1.6.8

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -5,8 +5,7 @@ import 'modules/home/telas_module.dart';
 class AppModule extends Module {
   @override
   void routes(r) {
-    r.module('/', module: TelasModule());
-    r.module('//', module: TelasModule());    
+    r.module('/', module: TelasModule());   
     r.wildcard(child: (_) => const NotFoundPage());
   }
 }
