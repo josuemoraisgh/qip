50ce1cda4b6aba7c8964094fe733540e30572c6a
josuemoraisgh <josuemorais@gmail.com>
2025-06-24 11:58:27 -0300

    refactor python

---
+++ python/install.bat
@@ -0,0 +1,7 @@
+CALL python.exe -m pip install --upgrade pip
+CALL pip install openpyxl
+CALL pip install pandas 
+CALL pip install matplotlib
+CALL pip install numpy 
+CALL pip install tensorflow
+CALL pip install scikit-learn
